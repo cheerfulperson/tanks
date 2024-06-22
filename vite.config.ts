c86fe6efe98f2167328e8388f76fe8017075de78
cheerfulperson <yagor.baradavkin@gmail.com>
2024-06-22 20:33:03 +0300

    init: added basic project configuration

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+      generateScopedName: "[name]-[hash:base64:2]__[local]"
+    }
+  },
+  plugins: [react(), tsconfigPaths()],
+})
