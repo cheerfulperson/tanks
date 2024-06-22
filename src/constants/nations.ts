50363f648cdb49eda93bfeaf0ad454a537e0628b
cheerfulperson <yagor.baradavkin@gmail.com>
2024-06-22 21:45:01 +0300

    feat: add constants

---
+++ src/constants/nations.ts
@@ -0,0 +1,41 @@
+import {
+  China,
+  Czech,
+  France,
+  Germany,
+  Italy,
+  Japan,
+  Poland,
+  Sweden,
+  UK,
+  USA,
+  USSR,
+} from "components/icons";
+
+export enum Nations {
+  USSR = "ussr",
+  USA = "usa",
+  GERMANY = "germany",
+  UK = "uk",
+  JAPAN = "japan",
+  CHINA = "china",
+  FRANCE = "france",
+  SWEDEN = "sweden",
+  ITALY = "italy",
+  POLAND = "poland",
+  CZECH = "czech",
+}
+
+export const NationsIcons = {
+  [Nations.USSR]: USSR,
+  [Nations.USA]: USA,
+  [Nations.GERMANY]: Germany,
+  [Nations.UK]: UK,
+  [Nations.JAPAN]: Japan,
+  [Nations.CHINA]: China,
+  [Nations.FRANCE]: France,
+  [Nations.SWEDEN]: Sweden,
+  [Nations.ITALY]: Italy,
+  [Nations.POLAND]: Poland,
+  [Nations.CZECH]: Czech,
+};
---
+++ src/constants/tanksTypes.ts
@@ -0,0 +1,7 @@
+export const TanksTypes = {
+  heavyTank: "heavyTank",
+  "AT-SPG": "AT-SPG",
+  mediumTank: "mediumTank",
+  lightTank: "lightTank",
+  SPG: "SPG",
+} as const;
